ec579046de371a01b3acbc546899534df4803e67
xujinfeng <1352176165@qq.com>
2022-05-27 14:24:19 +0800

    add @vitejs/plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
 });
